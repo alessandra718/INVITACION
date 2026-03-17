7d930c6801469bc2ba20aa99acead3cf0e2bc1ba
alessandra718 <153764466+alessandra718@users.noreply.github.com>
2026-03-17 12:22:37 -0500

    Update theme_common.js

--- js/theme_common.js
+++ js/theme_common.js
@@ -54,63 +54,57 @@
         }
 
 
-
-        function random_music_as(){  //获取随机的模板图片
-            // console.log('random_words_as'); 
-            var random_num=Math.floor(Math.random()*(array_as_music.length)); //随机取值 
+function random_music_as(){ 
+            var random_num=Math.floor(Math.random()*(array_as_music.length)); 
             var random_music=array_as_music[random_num];
             return random_music;
         }
 
-        //控制音乐切换播放暂停 
         var img_music=document.getElementById('img_music');
         var timeout_music;
-        function music_switch(){ //切换   
+        function music_switch(){ 
             clearTimeout(timeout_music);  
             if(audio_music.paused){
                 console.log('switch music to play');
                 audio_music.play();
-                audio_record.pause(); //播放音乐时录音一定暂停                
+                audio_record.pause();                 
                 img_music.style.webkitAnimation="music_play_rotate 1s linear infinite";
-                $(".div_music_tips").html("正播放").show();                
+                $(".div_music_tips").html("Reproduciendo").show(); // Cambio seguro
                 timeout_music=setTimeout(function(){$(".div_music_tips").hide()}, 2500);
             }else{
                 console.log('switch music to paused'); 
                 audio_music.pause();
-                // audio_record.play(); //
                 img_music.style.webkitAnimation="";
-                $(".div_music_tips").html("已暂停").show();  
+                $(".div_music_tips").html("Pausado").show(); // Cambio seguro 
                 timeout_music=setTimeout(function(){$(".div_music_tips").hide()}, 2500); 
             } 
         }
 
         var timeout_record;
         var div_record=document.getElementById('div_record');
-        function record_switch(){ //切换 
+        function record_switch(){ 
             clearTimeout(timeout_record);
             if(audio_record.paused){
                 console.log('switch record to play'); 
                 audio_record.play();
-                audio_music.pause(); //
+                audio_music.pause(); 
                 img_music.style.webkitAnimation="";
                 div_record.style.webkitAnimation="btn_rotate 1s linear infinite";
-                $(".div_record_tips").html("正播放").show(); 
+                $(".div_record_tips").html("Reproduciendo").show(); // Cambio seguro
                 timeout_record=setTimeout(function(){$(".div_record_tips").hide()}, 2500);
             }else{
                 console.log('switch record to pause');  
-                audio_record.pause(); //播放音乐时录音一定暂停
+                audio_record.pause(); 
                 audio_music.play();                
                 img_music.style.webkitAnimation="music_play_rotate 1s linear infinite";
                 div_record.style.webkitAnimation="";
-                $(".div_record_tips").html("已暂停").show();  
+                $(".div_record_tips").html("Pausado").show(); // Cambio seguro 
                 timeout_record=setTimeout(function(){$(".div_record_tips").hide()}, 2500); 
             } 
         }
 
 
-
        
-
         wx.ready(function(){
             console.log('wx.ready success to start');
             audio_music.play(); //触发音乐自动播放
